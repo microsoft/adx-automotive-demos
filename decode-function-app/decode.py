c6ad4b5d768268841c08ce232e6e70f534104310
Karim Hammouda <karim.hammoda@gmail.com>
2022-11-18 19:11:50 +0000

    Update decode.py
    New version after creating CSV

--- decode-function-app/decode.py
+++ decode-function-app/decode.py
@@ -1,24 +1,74 @@
+import json
+import csv
+import time
+import logging
+import tempfile
+import gzip
+import numpy as np
+import pandas as pd
+from asammdf import MDF
+from datetime import datetime, timedelta
 
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
 from azure.identity import DefaultAzureCredential
 
-import json
-import logging
-import time
-import tempfile
-import os
-    
 
+# Writes a gzipped CSV file using the uuid as name
+def writeCsv(csv_file, mdf, target_file):
+    # open the file in the write mode
+    with gzip.open(csv_file, 'wt') as csvFile:
 
-import numpy as np
-import pandas as pd
-from asammdf import MDF
-import csv
+        logging.info("Exporting to: " + csvFile.name + '-signalscsv.gz' )
+
+        writer = csv.writer(csvFile)
+        writer.writerow(["source_file","source_uuid", "name", "unit", "relativeTimestamp", "absoluteTimestamp", "value", "value_string", "source_type", "bus_type"])
+
+        # Start time of the recording
+        recordingStartTime = mdf.header.start_time
+
+        for signals in mdf.iter_channels():            
+
+            try:
+                numericSignals = signals.samples.astype(np.double)
+                stringSignals = np.empty(len(signals.timestamps), dtype=str)
+                importType = "numeric"
+            except:
+                numericSignals = np.full(len(signals.timestamps), dtype=np.double, fill_value=0)
+                stringSignals = signals.samples.astype(str)
+                importType = "string"
+
+            logging.info(f"Exporting signal: {signals.name} as type {importType}")               
+
+            for indx in range(0, len(signals.timestamps)):
+
+                try:
+                    numericValue = float(signals.samples[indx])
+                except:
+                    numericValue = "",
+
+                writer.writerow(
+                    [
+                        target_file,
+                        str(csv_file).rsplit('/')[-1],
+                        signals.name, 
+                        signals.unit, 
+                        signals.timestamps[indx],
+                        recordingStartTime + timedelta(seconds=signals.timestamps[indx]),
+                        numericSignals[indx],
+                        stringSignals[indx],
+                        signals.source.source_type,
+                        signals.source.bus_type
+                    ]
+                )
+
+    csvFile.close()
 
 
 
 def main(event: func.EventGridEvent):
+    
+    #boilerplated event trigger code
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -26,8 +76,6 @@ def main(event: func.EventGridEvent):
         'subject': event.subject,
         'event_type': event.event_type,
     })
-    
-    
     logging.info('Python EventGrid trigger processed an event: %s', result)
    
     #raw blob details
@@ -35,40 +83,57 @@ def main(event: func.EventGridEvent):
     raw_CONTAINERNAME= event.subject.rsplit('/')[4]
     raw_BLOBNAME= event.subject.rsplit('/')[-1]
 
-    logging.info('raw blob details are A: %s C: %s B: %s', raw_STORAGEACCOUNTURL, raw_CONTAINERNAME, raw_BLOBNAME)
-
-    #create local file to place hold downloaded blog
-    #LOCALFILENAME= event.subject.rsplit('/')[-1].replace('.mf4' , '_local.mf4')
-    LOCALFILEPATH = '/tmp/'
-    LOCALFILE = 'local.mf4'
-
+    # create temporary directory and file to placehold downloaded blob
     try:
         tempFilePath = tempfile.gettempdir()
-        LOCALFILE = tempfile.NamedTemporaryFile()
-        logging.info('file created locally')
+        mdf_LOCALFILE = tempfile.NamedTemporaryFile()
+        logging.info('mdf file created locally')
 
     except FileNotFoundError as e:
         logging.info('cannot create directory')
+        raise
 
 
     # Acquire a credential object
     credential = DefaultAzureCredential()
 
     #download from blob into local file
-    t1=time.time()
     blob_service_client_instance = BlobServiceClient(account_url=raw_STORAGEACCOUNTURL, credential=credential)
     try:
         blob_client_instance = blob_service_client_instance.get_blob_client(container=raw_CONTAINERNAME, blob=raw_BLOBNAME, snapshot=None)
     except ResourceNotFoundError as e:
        logging.info("No blob found.")
+       raise
 
-    with open(LOCALFILE.name, "wb") as mf4_blob:
+    t1=time.time()
+    with open(mdf_LOCALFILE.name, "wb") as mf4_blob:
         blob_data = blob_client_instance.download_blob()
         blob_data.readinto(mf4_blob)
     t2=time.time()
-    logging.info(("It takes %s seconds to download "+raw_BLOBNAME) % (t2 - t1))
+    logging.info(("It takes %s seconds to download mdf file: "+raw_BLOBNAME) % (t2 - t1))
 
     # parse mf4 blob
-    mf4 = MDF(LOCALFILE.name)
-    logging.info('MDF version 4 parsed successfully')
+    mdf = MDF(mdf_LOCALFILE.name)
+    logging.info('mdf version 4 parsed successfully')
 
+
+    # << TODO: conversion >>
+    
+    #curated blob details
+    curated_CONTAINERNAME= raw_CONTAINERNAME.replace('raw' , 'curated')
+    curated_BLOBNAME= raw_BLOBNAME.replace('.mf4' , '-signalscsv.gz')
+
+
+    # create temporary directory and file to placehold downloaded blob
+    try:
+        tempFilePath = tempfile.gettempdir()
+        csv_LOCALFILE = tempfile.NamedTemporaryFile()
+        logging.info('csv file created locally')
+
+    except FileNotFoundError as e:
+        logging.info('cannot create directory')
+        raise
+
+    writeCsv(csv_LOCALFILE.name, mdf, curated_BLOBNAME)
+
+    logging.info("CSV file created successfully")

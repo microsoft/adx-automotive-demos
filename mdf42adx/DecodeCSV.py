3361bcb80a5ce9eb20a72af0c823c5a1fb7c83f2
Mario Ortegon <mario@ortegon-cabrera.de>
2023-08-20 22:32:43 +0200

    Added raw values in an additional column, and added raw values as value when string column

--- mdf42adx/DecodeCSV.py
+++ mdf42adx/DecodeCSV.py
@@ -15,51 +15,55 @@ def processSignalAsCsv(counter, filename, signalMetadata, uuid, targetdir, black
     if signalMetadata["name"] in blacklistedSignals:
         return (f"pid {os.getpid()}", False, counter, f">>> Skipped: {signalMetadata}")
 
-
-
     # Get the signal group and channel index to load that specific signal ONLY
     group_index = signalMetadata["group_index"]
     channel_index = signalMetadata["channel_index"]
 
     # Open the MDF file and select a single signal
     mdf = MDF(filename)          
-    signals = mdf.select([(None, group_index, channel_index)])        
+    # We select a specific signal, both decoded and raw
+    decodedSignal = mdf.select(channels=[(None, group_index, channel_index)])[0]
+    rawSignal = mdf.select(channels=[(None, group_index, channel_index)], raw=True)[0]
     
-    print(f"pid {os.getpid()}: MDF file open signal {counter}: {signalMetadata['name']} - {filename} opened group {group_index}, channel {channel_index})")
-    
-    for signal in signals:
-        print(f"pid {os.getpid()}: Processing signal {counter}: {signal.name} group index {group_index} channel index {channel_index} with type {signal.samples.dtype}")   
+    print(f"pid {os.getpid()}: Processing signal {counter}: {decodedSignal.name} group index {group_index} channel index {channel_index} with type {decodedSignal.samples.dtype}")   
 
-        # open the file in the write mode
-        with gzip.open(os.path.join(targetdir, f"{uuid}-{counter}.csv.gz"), 'wt') as csvFile:
 
-            channel_group_acq_name, acq_source_name, acq_source_path = getSource(mdf, signal)    
-            numericSignals, stringSignals = extractSignalsByType(signal)
+    targetfile = os.path.join(targetdir, f"{uuid}-{counter}.csv.gz")
+    os.makedirs(os.path.dirname(targetfile), exist_ok=True)
 
-            writer = csv.writer(csvFile)
-            writer.writerow(["source_uuid", "name", "unit", "timestamp", "value", "value_string", "source", "channel_group_acq_name", "acq_source_name", "acq_source_path", "source_type", "bus_type"])                
-                            
-            # Iterate on the entries for the signal
-            for indx in range(0, len(signal.timestamps)):
+    # open the file in the write mode
+    with gzip.open(targetfile, 'wt') as csvFile:
 
-                writer.writerow(
-                    [
-                        str(uuid),
-                        signal.name, 
-                        signal.unit, 
-                        signal.timestamps[indx],
-                        numericSignals[indx],
-                        stringSignals[indx],
-                        signal.source.name,
-                        channel_group_acq_name,
-                        acq_source_name,
-                        acq_source_path,                    
-                        v4c.SOURCE_TYPE_TO_STRING[signal.source.source_type],
-                        v4c.BUS_TYPE_TO_STRING[signal.source.bus_type],
-                    ]
-                )
+        channel_group_acq_name, acq_source_name, acq_source_path = getSource(mdf, decodedSignal)    
+        numericSignals, stringSignals = extractSignalsByType(decodedSignal, rawSignal)
 
-        csvFile.close()
+        writer = csv.writer(csvFile)
+        writer.writerow(["source_uuid", "name", "unit", "timestamp", "value", "value_string", "value_raw", "source", "channel_group_acq_name", "acq_source_name", "acq_source_path", "source_type", "bus_type"])                
+                        
+        # Iterate on the entries for the signal
+        for indx in range(0, len(decodedSignal.timestamps)):
+
+            writer.writerow(
+                [
+                    str(uuid),
+                    decodedSignal.name, 
+                    decodedSignal.unit, 
+                    decodedSignal.timestamps[indx],
+                    numericSignals[indx],
+                    stringSignals[indx],
+                    rawSignal[indx],
+                    decodedSignal.source.name,
+                    channel_group_acq_name,
+                    acq_source_name,
+                    acq_source_path,                    
+                    v4c.SOURCE_TYPE_TO_STRING[decodedSignal.source.source_type],
+                    v4c.BUS_TYPE_TO_STRING[decodedSignal.source.bus_type],
+                ]
+            )
+
+    csvFile.close()
     
     end_signal_time = time.time() - start_signal_time
-    print(f"Signal {counter}: {signal.name} with {len(signal.timestamps)} entries took {end_signal_time}")
+    mdf.close()
+    
+    return (f"Signal {counter}: {decodedSignal.name} with {len(decodedSignal.timestamps)} entries took {end_signal_time}")

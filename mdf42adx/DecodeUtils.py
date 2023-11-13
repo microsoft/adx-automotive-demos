2d84b1202dd231c3c87c3d25a66797a28a0ceb01
flavien.daussy <flavien.daussy@gmail.com>
2023-11-13 15:29:45 +0000

    McLaren Commit #4: Replaces Commit #3 Corrected the return of string signals as utf-8 instead of "string" (and S32 in commit #3). Aimed at solving issue 71 --> https://github.com/microsoft/adx-automotive-demos/issues/71 and issue 63: https://github.com/microsoft/adx-automotive-demos/issues/63

--- mdf42adx/DecodeUtils.py
+++ mdf42adx/DecodeUtils.py
@@ -94,7 +94,7 @@ def extractSignalsByType(decodedSignal, rawSignal):
     # For everything else use the previous approach
     else:
         floatSignals = rawSignal.samples.astype(float)
-        stringSignals = decodedSignal.samples.astype("S32") #astype(string) was causing issues with special characters
+        stringSignals = decodedSignal.samples.view(np.chararray).decode('utf-8') #astype(string) was causing issues with special characters, and S32 would have truncated results.
 
     
 
